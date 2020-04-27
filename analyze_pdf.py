52a7e4fa2c5788bf49761ce14bf19675452a95dc
mima3 <7877000+mima3@users.noreply.github.com>
2020-04-27 10:15:19 +0900

    正常に取得できないPDFに対してcamelotのパラメータを微調整

--- analyze_pdf.py
+++ analyze_pdf.py
@@ -3,6 +3,7 @@ import json
 import camelot
 import csv
 import copy
+import mojimoji
 from tqdm import tqdm
 
 
@@ -12,6 +13,14 @@ class AnalyzePdf:
     page = 0
     rule = {}
 
+    def info(self, s):
+        self.log.append({
+             "kind" : "info",
+             "path" : self.target_path,
+             "page" : self.page,
+             "message" : s
+        })
+
     def warn(self, s):
         self.log.append({
              "kind" : "warn",
@@ -20,6 +29,16 @@ class AnalyzePdf:
              "message" : s
         })
 
+    def _check_empty_line(self, df, ix):
+        """指定の行が空行か確認する"""
+        result = True
+        for k in self.rule['columns']:
+           col_ix = self.rule['columns'][k]['index']
+           if df.loc[ix][col_ix]:
+               result = False
+               break
+        return result
+    
     def _parse_df(self, df):
         ret = []
         if len(df.columns.values) < len(self.rule['columns']):
@@ -35,13 +54,23 @@ class AnalyzePdf:
             else:
                 offset = self.rule['other_page_offset']
             if ix < offset:
-               continue
+                continue
+            if self._check_empty_line(df, ix):
+                self.info("空行のため処理をスキップします:{}".format(ix))
+                continue
             for k in self.rule['columns']:
                col_ix = self.rule['columns'][k]['index']
                v = df.loc[ix][col_ix]
-               if 'function' in self.rule['columns'][k]:
-                   method_to_call = getattr(self, self.rule['columns'][k]['function'])
-                   v = method_to_call(ix, k, v)
+               chk_error = ""
+               if 'chk_func' in self.rule['columns'][k]:
+                   chk_method = getattr(self, self.rule['columns'][k]['chk_func'])
+                   chk_error = chk_method(v)
+                   if chk_error:
+                       self.warn("{} index:{} col:{} value:{}".format(chk_error, ix, col_ix, v))
+
+               if 'cnv_func' in self.rule['columns'][k] and not chk_error:
+                   cnv_method = getattr(self, self.rule['columns'][k]['cnv_func'])
+                   v = cnv_method(v)
                rec[k] = v
 
             ret.append(rec)
@@ -49,7 +78,12 @@ class AnalyzePdf:
 
     def _parse_page(self):
         ret = []
-        tables = camelot.read_pdf(self.target_path, pages=str(self.page))
+        tables = camelot.read_pdf(
+            self.target_path,
+            pages=str(self.page),
+            line_scale = self.rule['line_scale'],
+            layout_kwargs={'char_margin': self.rule['char_margin']}
+        )
 
         if len(tables) == 0:
             return ret
@@ -75,27 +109,41 @@ class AnalyzePdf:
         return ret
 
 
-    def conv_oneline(self, row, col, s):
-       s = s.replace('\n', '')
-       s = s.replace('\r', '')
-       return s
+    def conv_oneline(self, s):
+        s = s.replace('\n', '')
+        s = s.replace('\r', '')
+        return s
 
-    def conv_required_oneline(self, row, col, s):
-       s = s.replace('\n', '')
-       s = s.replace('\r', '')
-       if not s:
-           self.warn("データが空です row:{} col:{}".format(row, col))
-       return s
+    def chk_required(self, s):
+        if not s.strip():
+            return "値が存在しません."
+        return ""
 
+    def conv_number(self, s):
+        s = self.conv_oneline(s)
+        s = mojimoji.zen_to_han(s)
+        s = s.strip()
+        s = s.replace('〒', '')
+        s = s.replace('-', '')
+        s = s.replace('ー', '')
+        s = s.replace('―', '')
+        s = s.replace('‐', '')
+        s = s.replace('－', '')
+        s = s.replace('−', '')
+        s = s.replace('ｰ', '')
+        s = s.replace('(', '')
+        s = s.replace(')', '')
+        return s
 
-    def conv_required_numcode_oneline(self, row, col, s):
-        ret = self.conv_oneline(row, col, s)
-        chk_str = ret.replace('-', '')
-        chk_str = chk_str.replace('ー', '')
-        chk_str = chk_str.replace('ー', '')
-        if not str.isdigit(chk_str):
-            self.warn("データが数値ではありません row:{} col:{} value:{}".format(row, col, s))
-        return ret
+    def conv_postal_code(self, s):
+        s = self.conv_number(s)
+        return s[0:3] + "-" + s[3:]
+
+    def chk_postal_code(self, s):
+        s = self.conv_number(s)
+        if not s.isdigit() or len(s) != 7:
+            return "郵便番号の形式ではありません"
+        return ""
 
 if __name__ == '__main__':
     argvs = sys.argv
@@ -111,27 +159,32 @@ if __name__ == '__main__':
     default_rule = {
        "first_page_offset" : 2,
        "other_page_offset" : 2,
+       "char_margin" : 0.5,          # 2つの文字がこのマージンよりも接近している場合、それらは同じ行の一部と見なされます。
        "accuracy" : 95.0,
+       "line_scale": 15,             # 大きいほど小さい線を検出するが、大きすぎると文字を線と見なしてしまう。
        "columns" : {
            "name" : {
                "index" : 1,
-               "function" : "conv_required_oneline"
+               "chk_func" : "chk_required",
+               "cnv_func" : "conv_oneline"
            },
            "postal_code" : {
                "index" : 2,
-               "function" : "conv_required_numcode_oneline"
+               "chk_func" : "chk_postal_code",
+               "cnv_func" : "conv_postal_code"
            },
            "address" : {
                "index" : 3,
-               "function" : "conv_required_oneline"
+               "chk_func" : "chk_required",
+               "cnv_func" : "conv_oneline"
            },
            "tel": {
                "index" : 4, 
-               "function" : "conv_required_numcode_oneline"
+               "chk_func" : "chk_required"
            },
            "url": {
                "index" : 5, 
-               "function" : "conv_oneline"
+               "cnv_func" : "conv_oneline"
            },
            "first": {
                "index" : 6, 
@@ -158,16 +211,12 @@ if __name__ == '__main__':
     rules['宮崎県']['other_page_offset'] = 0
     rules['京都府'] = copy.deepcopy(default_rule)
     rules['京都府']['other_page_offset'] = 0
-    rules['山梨県'] = copy.deepcopy(default_rule)
-    rules['山梨県']['other_page_offset'] = 0
     rules['鹿児島県'] = copy.deepcopy(default_rule)
     rules['鹿児島県']['other_page_offset'] = 0
     rules['千葉県'] = copy.deepcopy(default_rule)
     rules['千葉県']['other_page_offset'] = 0
     rules['富山県'] = copy.deepcopy(default_rule)
     rules['富山県']['other_page_offset'] = 0
-    rules['和歌山県'] = copy.deepcopy(default_rule)
-    rules['和歌山県']['other_page_offset'] = 0
     rules['香川県'] = copy.deepcopy(default_rule)
     rules['香川県']['other_page_offset'] = 0
     rules['佐賀県'] = copy.deepcopy(default_rule)
@@ -208,12 +257,32 @@ if __name__ == '__main__':
     rules['奈良県']['columns']['doctor']['index'] = 10
     rules['奈良県']['columns']['cooperation']['index'] = 11
     #
-    rules['神奈川県'] = copy.deepcopy(default_rule)
-    rules['神奈川県']['accuracy'] = 90.0
     rules['三重県'] = copy.deepcopy(default_rule)
     rules['三重県']['accuracy'] = 90.0
     rules['徳島県'] = copy.deepcopy(default_rule)
     rules['徳島県']['accuracy'] = 90.0
+    #
+    # URLとTELが結合するため調整が必要
+    #rules['和歌山県'] = copy.deepcopy(default_rule)
+    #rules['和歌山県']['other_page_offset'] = 0
+    #rules['和歌山県']['char_margin'] = 0.05
+    #rules['広島県'] = copy.deepcopy(default_rule)
+    #rules['広島県']['char_margin'] = 0.25    
+    #
+    rules['山梨県'] = copy.deepcopy(default_rule)
+    rules['山梨県']['other_page_offset'] = 0
+    rules['山梨県']['line_scale'] = 30
+    rules['山梨県']['char_margin'] = 0.25
+    #
+    rules['神奈川県'] = copy.deepcopy(default_rule)
+    rules['神奈川県']['accuracy'] = 90.0
+    rules['神奈川県']['char_margin'] = 0.25
+    #
+    rules['新潟県'] = copy.deepcopy(default_rule)
+    rules['新潟県']['char_margin'] = 0.25
+    #
+    rules['長野県'] = copy.deepcopy(default_rule)
+    rules['長野県']['char_margin'] = 0.25
 
     for data in pdf_info["list"]:
         print(data['local'])
